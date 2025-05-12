683b835a66869b73cb1767378ed310de4f821cee
AzraFajriansyah <fajriansyah45@gmail.com>
2025-05-12 21:37:56 +0700

    edited again the js so it will save on localStorage

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 To-Do List App
 
+11 May 2025
 Basically, it contains index.html for structure, style/main.css for styling, scripts/app.js for logic and assets/ for I'm having icons, pictures or fonts.
 
-For now, it have the form to input the task list, add task button and the task will appeared after add the button
-
-On this update, I added delete button for task, and if it clicked then the task will deleted
+12 May 2025
+For now, it have the form to input the task list, add task button and the task will appeared after add the button. I also added delete button for task, and if it clicked then the task will deleted. And I edited the js again so it can save the task in the Local Storage
--- scripts/app.js
+++ scripts/app.js
@@ -2,10 +2,20 @@ const taskInput = document.getElementById("task-input");
 const addTaskBtn = document.getElementById("add-task-btn");
 const taskList = document.getElementById("task-list");
 
+// Ambil data dari localStorage saat halaman dibuka
+window.addEventListener("load", loadTasks);
+
 addTaskBtn.addEventListener("click", () => {
     const taskText = taskInput.value.trim();
 
     if (taskText !== "") {
+        addTaskToList(taskText);
+        saveTask(taskText);
+        taskInput.value = "";
+    }
+});
+
+    function addTaskToList(taskText){
         const taskItem = document.createElement("div");
         taskItem.className = "task-item";
         
@@ -18,12 +28,27 @@ addTaskBtn.addEventListener("click", () => {
 
         deleteBtn.addEventListener("click", () => {
             taskList.removeChild(taskItem);
+            removeTask(taskText);
         });
 
         taskItem.appendChild(textSpan);
         taskItem.appendChild(deleteBtn);
         taskList.appendChild(taskItem);
-        
-        taskInput.value = "";
     }
-});
+
+    function saveTask(taskText) {
+        let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+        tasks.push(taskText);
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+
+    function loadTasks() {
+        let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+        tasks.forEach(task => addTaskToList(task));
+    }
+
+    function removeTask(taskText) {
+        let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+        tasks = tasks.filter(task => task !== taskText);
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
